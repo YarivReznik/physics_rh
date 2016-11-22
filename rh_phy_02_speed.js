e02ef44742acf35c4018fc3e4f2d6b03ff2927aa
yariv <yariv@spectory.com>
2016-11-22 23:35:29 +0200

    drag

--- rh_phy_02_speed.js
+++ rh_phy_02_speed.js
@@ -1,4 +1,4 @@
-particle_rh = function (id,pos, vl, ac, drag) {
+particle_rh = function (id,pos, v, a) {
   if(id === undefined){
     return
   }
@@ -6,26 +6,29 @@ particle_rh = function (id,pos, vl, ac, drag) {
   particle.id = id ;
   particle.d = {
     pos: pos || [0, 0],
-    vl: vl || [0, 0],
-    ac: ac || [0, 0],
-    drag: drag || 0.1
+    v: v || [0, 0],
+    a: a || [0, 0]
   };
-  particle.active = false;
-  particle.addV = function (x, y) {
-    particle.pos[0] += x || 0;
-    particle.pos[0] += y || 0;
+  particle.active = true;
+  particle.addV = function (vec) {
+    particle.d.pos[0] += vec[0] || 0;
+    particle.d.pos[1] += vec[1] || 0;
   };
-
+  particle.update = function(){
+    particle.addV(particle.d.v);
+  }
   return particle;
 };
-var NUM_OF_PARTICELS = 10;
+var NUM_OF_PARTICELS = 100;
 var center_x = window.innerWidth/2;
 var center_y = window.innerHeight/2;
 
 var dum_arr = new Array(NUM_OF_PARTICELS);
 var particles = {};
+var spread = 0;
 _.each(dum_arr,function(val,key){
-  particles[key] = particle_rh(key,[_.random(-100,100)+center_x, _.random(-100,100)+center_y])
+  particles[key] = particle_rh(key,[_.random(-spread,spread)+center_x, _.random(-spread,spread)+center_y]);
+  particles[key].d.v = [_.random(-1.1,1.1),_.random(-1.1,1.1)];
 });
 
 console.log(particles);
@@ -46,6 +49,7 @@ window.onload = function() {
       ctx.beginPath();
       ctx.arc(particle.d.pos[0],particle.d.pos[1],5,0,2*Math.PI);
       ctx.stroke();
+      particle.update();
     })
 
     requestAnimationFrame(update);
--- rh_phy_03_drag.js
+++ rh_phy_03_drag.js
@@ -1,20 +1,35 @@
-particle_rh = function (id,pos, vl, ac) {
+particle_rh = function (id,pos, v, a) {
   if(id === undefined){
     return
   }
+  var drag = 0.01;
   var particle = {};
   particle.id = id ;
   particle.d = {
     pos: pos || [0, 0],
-    vl: vl || [0, 0],
-    ac: ac || [0, 0]
+    v: v || [0, 0],
+    a: a || [0, 0],
   };
+  particle.setDrag = function(num){
+    if(num < 0 || num >1){
+      return
+    }
+    drag = num;
+  }
   particle.active = true;
   particle.addV = function (vec) {
     particle.d.pos[0] += vec[0] || 0;
     particle.d.pos[1] += vec[1] || 0;
   };
-
+  particle.applyDrag = function(){
+    particle.d.v[0] =1;
+  }
+  particle.update = function(){
+    //apply drag
+    particle.d.v[0] = particle.d.v[0] - particle.d.v[0]*drag;
+    particle.d.v[1] = particle.d.v[1] - particle.d.v[1]*drag;
+    particle.addV(particle.d.v);
+  }
   return particle;
 };
 var NUM_OF_PARTICELS = 100;
@@ -23,8 +38,11 @@ var center_y = window.innerHeight/2;
 
 var dum_arr = new Array(NUM_OF_PARTICELS);
 var particles = {};
+var spread = 50;
 _.each(dum_arr,function(val,key){
-  particles[key] = particle_rh(key,[_.random(-100,100)+center_x, _.random(-100,100)+center_y])
+  particles[key] = particle_rh(key,[_.random(-spread,spread)+center_x, _.random(-spread,spread)+center_y]);
+  particles[key].d.v = [_.random(-3.1,3.1),_.random(-3.1,3.1)];
+
 });
 
 console.log(particles);
@@ -45,6 +63,7 @@ window.onload = function() {
       ctx.beginPath();
       ctx.arc(particle.d.pos[0],particle.d.pos[1],5,0,2*Math.PI);
       ctx.stroke();
+      particle.update();
     })
 
     requestAnimationFrame(update);
